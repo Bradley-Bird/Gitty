f543bf8719f0a7ebc8313327cbea1a3a7a9fb925
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-26 21:43:46 -0700

    started to make post tests, realized I needed to mock sign in to post. no tests passing currently.

---
+++ __tests__/github.test.js
@@ -0,0 +1,6 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/github.js');
---
+++ __tests__/posts.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Post = require('../lib/models/Post');
+
+describe('post routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end;
+  });
+
+  it('should add a new post ', async () => {
+    const post = new Post({
+      post: 'hello world',
+      user_id: 1,
+    });
+    const resp = await request(app).post('/posts').send(post);
+    console.log('resp', resp);
+    expect(resp.body.post).toEqual(post.post);
+  });
+});
